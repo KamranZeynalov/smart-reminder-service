4a2ed4e87339585b71479ac8ff561e68db0e08e9
KamranZeynalov <zeynalovkamran2@gmail.com>
2025-07-24 17:31:14 +0400

    sam template v1 created

---
+++ src/main/java/com/company/service/ReminderSetupFunction.java
@@ -0,0 +1,14 @@
+package com.company.service;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class ReminderSetupFunction implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+        return null;
+    }
+}
